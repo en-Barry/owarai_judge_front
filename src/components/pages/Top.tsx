3f6e0f512e4fb52ac32ae2f1a3621dc9974a9548
en-Barry <en_barry@icloud.com>
2021-08-30 05:42:03 +0900

    Add: トップページ画面に必要なボタン群を追加

--- src/components/pages/Top.tsx
+++ src/components/pages/Top.tsx
@@ -1,7 +1,33 @@
-import { memo, VFC } from "react";
+/* eslint-disable  react-hooks/exhaustive-deps */
+import { memo, useCallback, VFC } from "react";
+import { Button, Container, Heading } from "@chakra-ui/react";
+import { useHistory } from "react-router-dom";
+import { FaTwitter } from "react-icons/fa";
 
 export const Top: VFC = memo(() => {
+  const history = useHistory();
+
+  const onClickM1 = useCallback(() => history.push('/selection/m-1gp'), []);
+  const onClickKing = useCallback(() => history.push('/selection/king-of-conte'), []);
+  const onClickR1 = useCallback(() => history.push('/selection/r-1gp'), []);
+
   return (
-    <p>TOPページです</p>
+    <>
+    <Heading align='center' mb={6}>どの審査員になる？</Heading>
+      <Container align='center'>
+        <Button w='80%' mb={6} p={6} fontSize='lg' bg='#424242' color='#DAAA00' onClick={onClickM1}>
+          M-1グランプリ
+        </Button>
+        <Button w='80%' mb={6} p={6} fontSize='lg' bg='#262626' color='#DDDDDD' onClick={onClickKing}>
+          キングオブコント
+        </Button>
+        <Button w='80%' mb={6} p={6} fontSize='lg' bg='#002D71' color='#DAAA00' onClick={onClickR1}>
+          R-1グランプリ
+        </Button>
+        <Button colorScheme="twitter" leftIcon={<FaTwitter />} mb={6}>
+          Twitterでシェアする
+        </Button>
+      </Container>
+    </>
   )
 });

db253dd30173d5c4a177694cecd7b48c3d876e07
en-Barry <en_barry@icloud.com>
2021-08-30 01:13:20 +0900

    Add: ヘッダー部分のルーティングを設定
    ヘッダーのリンクが押されたときに特定のページをレンダリングするため

--- src/components/molecules/MenuDrawer.tsx
+++ src/components/molecules/MenuDrawer.tsx
@@ -4,19 +4,23 @@ import { Button, Drawer, DrawerBody, DrawerContent, DrawerOverlay } from "@chakr
 type Props = {
   onClose: () => void;
   isOpen: boolean;
-}
+  onClickTop: () => void;
+  onClickM1: () => void;
+  onClickKing: () => void;
+  onClickR1: () => void;
+};
 
 export const MenuDrawer: VFC<Props> = memo((props) => {
-  const { onClose, isOpen } = props;
+  const { onClose, isOpen, onClickTop, onClickM1, onClickKing, onClickR1 } = props;
   return (
     <Drawer placement='left' size='xs' onClose={onClose} isOpen={isOpen}>
       <DrawerOverlay>
         <DrawerContent>
           <DrawerBody p={0} bg='gray.100'>
-            <Button w='100%'>TOP</Button>
-            <Button w='100%'>M-1グランプリ</Button>
-            <Button w='100%'>キングオブコント</Button>
-            <Button w='100%'>R-1グランプリ</Button>
+            <Button w='100%' onClick={onClickTop}>TOP</Button>
+            <Button w='100%' onClick={onClickM1}>M-1グランプリ</Button>
+            <Button w='100%' onClick={onClickKing}>キングオブコント</Button>
+            <Button w='100%' onClick={onClickR1}>R-1グランプリ</Button>
           </DrawerBody>
         </DrawerContent>
       </DrawerOverlay>
--- src/components/organisms/layout/Header.tsx
+++ src/components/organisms/layout/Header.tsx
@@ -1,11 +1,19 @@
+/* eslint-disable  react-hooks/exhaustive-deps */
 import { Box, Flex, Heading, Link, useDisclosure } from "@chakra-ui/react";
-import { memo, VFC } from "react";
+import { memo, useCallback, VFC } from "react";
+import { useHistory } from "react-router-dom";
 
 import { MenuIconButton } from "../../atoms/button/MenuIconButton";
 import { MenuDrawer } from "../../molecules/MenuDrawer";
 
 export const Header: VFC = memo(() => {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const history = useHistory();
+
+  const onClickTop = useCallback(() => history.push('/'), []);
+  const onClickM1 = useCallback(() => history.push('/selection/m-1gp'), []);
+  const onClickKing = useCallback(() => history.push('/selection/king-of-conte'), []);
+  const onClickR1 = useCallback(() => history.push('/selection/r-1gp'), []);
 
   return (
     <>
@@ -15,23 +23,28 @@ export const Header: VFC = memo(() => {
             align='center'
             justify='space-between'
             padding={{ base: 4, md: 5 }}>
-        <Flex align='center' as='a' mr={8} _hover={{ cursor: 'pointer'}}>
+        <Flex align='center' as='a' mr={8} _hover={{ cursor: 'pointer'}} onClick={onClickTop}>
           <Heading as='h1' fontSize={{ base: 'md', md: 'lg' }}>オワライジャッジ.com</Heading>
         </Flex>
         <Flex align='center' fontSize='sm' flexGrow={2} display={{ base: 'none', md: 'flex' }}>
           <Box pr={4}>
-            <Link>M-1グランプリ</Link>
+            <Link onClick={onClickM1}>M-1グランプリ</Link>
           </Box>
           <Box pr={4}>
-            <Link>キングオブコント</Link>
+            <Link onClick={onClickKing}>キングオブコント</Link>
           </Box>
           <Box>
-            <Link>R-1グランプリ</Link>
+            <Link onClick={onClickR1}>R-1グランプリ</Link>
           </Box>
         </Flex>
         <MenuIconButton onOpen={ onOpen }/>
       </Flex>
-      <MenuDrawer  onClose={ onClose } isOpen={ isOpen }  />
+      <MenuDrawer onClose={ onClose }
+                  isOpen={ isOpen }
+                  onClickTop={ onClickTop } 
+                  onClickM1={ onClickM1 }
+                  onClickKing= { onClickKing }
+                  onClickR1={ onClickR1 } />
     </>
   )
 });

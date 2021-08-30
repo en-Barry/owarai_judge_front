3662944c030f9b37431355734d62016ba132675e
en-Barry <en_barry@icloud.com>
2021-08-30 23:17:43 +0900

    Add: Selectionページのベースを作成

--- src/components/pages/SelectionKing.tsx
+++ src/components/pages/SelectionKing.tsx
@@ -1,7 +1,20 @@
 import { memo, VFC } from "react";
+import { Container, Heading } from "@chakra-ui/react";
+import styled from "styled-components";
+import { BaseButton } from "../atoms/button/BaseButton";
 
 export const SelectionKing: VFC = memo(() => {
   return (
-    <p>キングオブコントの年選択ページです</p>
+    <>
+      <Heading align='center' mb={6}>年代を選択する</Heading>
+        <Container align='center'>
+          <SButton>2021</SButton>
+        </Container>
+    </>
   )
 });
+
+const SButton = styled(BaseButton)`
+  background-color: #262626;
+  color: #DDDDDD;
+`;

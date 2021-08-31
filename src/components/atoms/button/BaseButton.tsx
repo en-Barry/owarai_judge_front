33fa545d072c1600d53d1caca13116edf4cd7169
en-Barry <en_barry@icloud.com>
2021-08-31 18:43:01 +0900

    Add: ボタンのベーススタイルを追加
    特定のスタイルを指定することで再利用しやすくするため

--- src/components/atoms/button/BaseButton.tsx
+++ src/components/atoms/button/BaseButton.tsx
@@ -2,9 +2,10 @@ import styled from 'styled-components';
 
 export const BaseButton = styled.button`
 	color: #fff;
+	width: 80%;
 	padding: 6px 24px;
 	border: none;
-	border-radius: 9999px;
+	border-radius: 8px;
 	outline: none;
 	&:hover {
 		cursor: pointer;

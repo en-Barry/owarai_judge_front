155c196a522d2ac4ac2764767f8824fc69f32717
en-Barry <en_barry@icloud.com>
2021-09-03 12:49:00 +0900

    Add: 年代選択画面のボタン群をコンポーネント化
    各コンテストで同じ表示方法をとるため

---
+++ src/components/molecules/SelectYearButtons.tsx
@@ -0,0 +1,34 @@
+/* eslint-disable  react-hooks/exhaustive-deps */
+import { memo, useCallback, VFC } from "react";
+import { useHistory } from "react-router-dom";
+
+import { BaseButton } from "../atoms/button/BaseButton";
+import { ContestType } from "../types/contest";
+
+type Props = {
+  contests: Array<ContestType>;
+  pathname: string;
+  bgColor: string;
+  color: string;
+}
+
+export const SelectYearButtons: VFC<Props> = memo((props) => {
+  const { contests, pathname, bgColor, color } = props;
+
+  const history = useHistory();
+  const onClickYear = useCallback((contest) => history.push({ pathname: `${pathname}`, state: { contest: contest }}), []);
+
+  return (
+    <>
+      {contests.map((contest) => (
+        <>
+        <BaseButton style={{color: `#${color}`, backgroundColor: `#${bgColor}`, marginBottom: '1rem'}}
+                    key={contest.id} onClick={() => onClickYear(contest)}>
+          {contest.year}
+        </BaseButton>
+        <br />
+        </>
+      ))}
+    </>
+  )
+});

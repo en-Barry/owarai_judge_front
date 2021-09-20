ea73220b6837556b1e063a7b8a5b5dd2fd50c669
en-Barry <en_barry@icloud.com>
2021-09-20 20:55:38 +0900

    Add: ファイナリスト取得時の例外処理を追加

--- src/components/hooks/useFinalist.ts
+++ src/components/hooks/useFinalist.ts
@@ -8,9 +8,13 @@ export const useFinalist = (props: string) => {
   const [finalists, setFinalists] = useState<Array<FinalistType>>([]);
 
   const getFinalists = useCallback(() => {
-    axios.get<Array<FinalistType>>(`http://localhost:3001/api/v1/finalists/${props}`).then((res) => {
-      setFinalists(res.data);
-    });
+    axios.get<Array<FinalistType>>(`http://localhost:3001/api/v1/finalists/${props}`, {withCredentials: true}).then((res) => {
+      if (res.data) {
+        setFinalists(res.data);
+      } else {
+        alert('ただいま準備中です');
+      }
+    }).catch(() => alert('情報が取得できませんでした'));
   }, []);
   return { getFinalists, finalists }
 };

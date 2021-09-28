2f55ca6bd0b212f6e5fd78e90194b321ac108937
en-Barry <en_barry@icloud.com>
2021-09-29 03:41:11 +0900

    Add: 審査結果画面の出力が完了

---
+++ src/components/molecules/ResultTable.tsx
@@ -0,0 +1,306 @@
+import { Container, Divider, Table, TableCellProps, TableColumnHeaderProps, TableRowProps, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/react";
+import { FC } from "react";
+import { memo, VFC } from "react";
+import { JudgementType2 } from "../types/judgement";
+
+type Props = {
+  other: JudgementType2;
+  finalists: JudgementType2;
+  judges: JudgementType2;
+  judgeResults2: JudgementType2[];
+}
+
+const STr : FC<TableRowProps> = (props) => <Tr backgroundColor='gray.700' {...props} />
+const STh : FC<TableColumnHeaderProps> = (props) => <Th color='white' {...props} />
+const STd : FC<TableCellProps> = (props) => <Td color='yellow.400' fontWeight='bold' {...props} />
+const FTh : FC<TableColumnHeaderProps> = (props) => <Th bg='white' color='gray.700' colSpan={7} textAlign='center' fontSize={18} {...props} />
+
+export const ResultTable: VFC<Props> = memo((props) => {
+  const { other, finalists, judges, judgeResults2 } = props;
+
+  return (
+    <>
+    <Container align='center'>
+      <Table size='sm'>
+        <Thead>
+          <Tr>
+            <FTh>{finalists.finalists_name[0].name}</FTh>
+          </Tr>
+          <STr>
+            <STh>{judges.judges_name[4].name}</STh>
+            <STh>{judges.judges_name[3].name}</STh>
+            <STh>{judges.judges_name[2].name}</STh>
+            <STh>{judges.judges_name[1].name}</STh>
+            <STh>{judges.judges_name[0].name}</STh>
+            <STh>国民</STh>
+            <STh>あなた</STh>
+          </STr>
+        </Thead>
+        <Tbody>
+          <STr>
+            <STd>{judgeResults2[4].score}</STd>
+            <STd>{judgeResults2[3].score}</STd>
+            <STd>{judgeResults2[2].score}</STd>
+            <STd>{judgeResults2[1].score}</STd>
+            <STd>{judgeResults2[0].score}</STd>
+            <STd>{other.other_results[0].score}</STd>
+            <STd>{judgeResults2[50].score}</STd>
+          </STr>
+        </Tbody>
+      </Table>
+      <Divider />
+      <Table size='sm'>
+        <Thead>
+          <Tr>
+            <FTh>{finalists.finalists_name[1].name}</FTh>
+          </Tr>
+          <STr>
+            <STh>{judges.judges_name[4].name}</STh>
+            <STh>{judges.judges_name[3].name}</STh>
+            <STh>{judges.judges_name[2].name}</STh>
+            <STh>{judges.judges_name[1].name}</STh>
+            <STh>{judges.judges_name[0].name}</STh>
+            <STh>国民</STh>
+            <STh>あなた</STh>
+          </STr>
+        </Thead>
+        <Tbody>
+          <STr>
+            <STd>{judgeResults2[9].score}</STd>
+            <STd>{judgeResults2[8].score}</STd>
+            <STd>{judgeResults2[7].score}</STd>
+            <STd>{judgeResults2[6].score}</STd>
+            <STd>{judgeResults2[5].score}</STd>
+            <STd>{other.other_results[1].score}</STd>
+            <STd>{judgeResults2[51].score}</STd>
+          </STr>
+        </Tbody>
+      </Table>
+      <Divider />
+      <Table size='sm'>
+        <Thead>
+          <Tr>
+            <FTh>{finalists.finalists_name[2].name}</FTh>
+          </Tr>
+          <STr>
+            <STh>{judges.judges_name[4].name}</STh>
+            <STh>{judges.judges_name[3].name}</STh>
+            <STh>{judges.judges_name[2].name}</STh>
+            <STh>{judges.judges_name[1].name}</STh>
+            <STh>{judges.judges_name[0].name}</STh>
+            <STh>国民</STh>
+            <STh>あなた</STh>
+          </STr>
+        </Thead>
+        <Tbody>
+          <STr>
+            <STd>{judgeResults2[14].score}</STd>
+            <STd>{judgeResults2[13].score}</STd>
+            <STd>{judgeResults2[12].score}</STd>
+            <STd>{judgeResults2[11].score}</STd>
+            <STd>{judgeResults2[10].score}</STd>
+            <STd>{other.other_results[2].score}</STd>
+            <STd>{judgeResults2[52].score}</STd>
+          </STr>
+        </Tbody>
+      </Table>
+
+      <Table size='sm'>
+        <Thead>
+          <Tr>
+            <FTh>{finalists.finalists_name[3].name}</FTh>
+          </Tr>
+          <STr>
+            <STh>{judges.judges_name[4].name}</STh>
+            <STh>{judges.judges_name[3].name}</STh>
+            <STh>{judges.judges_name[2].name}</STh>
+            <STh>{judges.judges_name[1].name}</STh>
+            <STh>{judges.judges_name[0].name}</STh>
+            <STh>国民</STh>
+            <STh>あなた</STh>
+          </STr>
+        </Thead>
+        <Tbody>
+          <STr>
+            <STd>{judgeResults2[19].score}</STd>
+            <STd>{judgeResults2[18].score}</STd>
+            <STd>{judgeResults2[17].score}</STd>
+            <STd>{judgeResults2[16].score}</STd>
+            <STd>{judgeResults2[15].score}</STd>
+            <STd>{other.other_results[3].score}</STd>
+            <STd>{judgeResults2[53].score}</STd>
+          </STr>
+        </Tbody>
+      </Table>
+
+      <Table size='sm'>
+        <Thead>
+          <Tr>
+            <FTh>{finalists.finalists_name[4].name}</FTh>
+          </Tr>
+          <STr>
+            <STh>{judges.judges_name[4].name}</STh>
+            <STh>{judges.judges_name[3].name}</STh>
+            <STh>{judges.judges_name[2].name}</STh>
+            <STh>{judges.judges_name[1].name}</STh>
+            <STh>{judges.judges_name[0].name}</STh>
+            <STh>国民</STh>
+            <STh>あなた</STh>
+          </STr>
+        </Thead>
+        <Tbody>
+          <STr>
+            <STd>{judgeResults2[24].score}</STd>
+            <STd>{judgeResults2[23].score}</STd>
+            <STd>{judgeResults2[22].score}</STd>
+            <STd>{judgeResults2[21].score}</STd>
+            <STd>{judgeResults2[20].score}</STd>
+            <STd>{other.other_results[4].score}</STd>
+            <STd>{judgeResults2[54].score}</STd>
+          </STr>
+        </Tbody>
+      </Table>
+      
+      <Table size='sm'>
+        <Thead>
+          <Tr>
+            <FTh>{finalists.finalists_name[5].name}</FTh>
+          </Tr>
+          <STr>
+            <STh>{judges.judges_name[4].name}</STh>
+            <STh>{judges.judges_name[3].name}</STh>
+            <STh>{judges.judges_name[2].name}</STh>
+            <STh>{judges.judges_name[1].name}</STh>
+            <STh>{judges.judges_name[0].name}</STh>
+            <STh>国民</STh>
+            <STh>あなた</STh>
+          </STr>
+        </Thead>
+        <Tbody>
+          <STr>
+            <STd>{judgeResults2[29].score}</STd>
+            <STd>{judgeResults2[28].score}</STd>
+            <STd>{judgeResults2[27].score}</STd>
+            <STd>{judgeResults2[26].score}</STd>
+            <STd>{judgeResults2[25].score}</STd>
+            <STd>{other.other_results[5].score}</STd>
+            <STd>{judgeResults2[55].score}</STd>
+          </STr>
+        </Tbody>
+      </Table>
+
+      <Table size='sm'>
+        <Thead>
+          <Tr>
+            <FTh>{finalists.finalists_name[6].name}</FTh>
+          </Tr>
+          <STr>
+            <STh>{judges.judges_name[4].name}</STh>
+            <STh>{judges.judges_name[3].name}</STh>
+            <STh>{judges.judges_name[2].name}</STh>
+            <STh>{judges.judges_name[1].name}</STh>
+            <STh>{judges.judges_name[0].name}</STh>
+            <STh>国民</STh>
+            <STh>あなた</STh>
+          </STr>
+        </Thead>
+        <Tbody>
+          <STr>
+            <STd>{judgeResults2[34].score}</STd>
+            <STd>{judgeResults2[33].score}</STd>
+            <STd>{judgeResults2[32].score}</STd>
+            <STd>{judgeResults2[31].score}</STd>
+            <STd>{judgeResults2[30].score}</STd>
+            <STd>{other.other_results[6].score}</STd>
+            <STd>{judgeResults2[56].score}</STd>
+          </STr>
+        </Tbody>
+      </Table>
+
+      <Table size='sm'>
+        <Thead>
+          <Tr>
+            <FTh>{finalists.finalists_name[7].name}</FTh>
+          </Tr>
+          <STr>
+            <STh>{judges.judges_name[4].name}</STh>
+            <STh>{judges.judges_name[3].name}</STh>
+            <STh>{judges.judges_name[2].name}</STh>
+            <STh>{judges.judges_name[1].name}</STh>
+            <STh>{judges.judges_name[0].name}</STh>
+            <STh>国民</STh>
+            <STh>あなた</STh>
+          </STr>
+        </Thead>
+        <Tbody>
+          <STr>
+            <STd>{judgeResults2[39].score}</STd>
+            <STd>{judgeResults2[38].score}</STd>
+            <STd>{judgeResults2[37].score}</STd>
+            <STd>{judgeResults2[36].score}</STd>
+            <STd>{judgeResults2[35].score}</STd>
+            <STd>{other.other_results[7].score}</STd>
+            <STd>{judgeResults2[57].score}</STd>
+          </STr>
+        </Tbody>
+      </Table>
+
+      <Table size='sm'>
+        <Thead>
+          <Tr>
+            <FTh>{finalists.finalists_name[8].name}</FTh>
+          </Tr>
+          <STr>
+            <STh>{judges.judges_name[4].name}</STh>
+            <STh>{judges.judges_name[3].name}</STh>
+            <STh>{judges.judges_name[2].name}</STh>
+            <STh>{judges.judges_name[1].name}</STh>
+            <STh>{judges.judges_name[0].name}</STh>
+            <STh>国民</STh>
+            <STh>あなた</STh>
+          </STr>
+        </Thead>
+        <Tbody>
+          <STr>
+            <STd>{judgeResults2[44].score}</STd>
+            <STd>{judgeResults2[43].score}</STd>
+            <STd>{judgeResults2[42].score}</STd>
+            <STd>{judgeResults2[41].score}</STd>
+            <STd>{judgeResults2[40].score}</STd>
+            <STd>{other.other_results[8].score}</STd>
+            <STd>{judgeResults2[58].score}</STd>
+          </STr>
+        </Tbody>
+      </Table>
+      
+      <Table size='sm'>
+        <Thead>
+          <Tr>
+            <FTh>{finalists.finalists_name[9].name}</FTh>
+          </Tr>
+          <STr>
+            <STh>{judges.judges_name[4].name}</STh>
+            <STh>{judges.judges_name[3].name}</STh>
+            <STh>{judges.judges_name[2].name}</STh>
+            <STh>{judges.judges_name[1].name}</STh>
+            <STh>{judges.judges_name[0].name}</STh>
+            <STh>国民</STh>
+            <STh>あなた</STh>
+          </STr>
+        </Thead>
+        <Tbody>
+          <STr>
+            <STd>{judgeResults2[49].score}</STd>
+            <STd>{judgeResults2[48].score}</STd>
+            <STd>{judgeResults2[47].score}</STd>
+            <STd>{judgeResults2[46].score}</STd>
+            <STd>{judgeResults2[45].score}</STd>
+            <STd>{other.other_results[9].score}</STd>
+            <STd>{judgeResults2[59].score}</STd>
+          </STr>
+        </Tbody>
+      </Table>
+    </Container>
+    </>
+  )
+});
--- src/components/pages/ResultKing.tsx
+++ src/components/pages/ResultKing.tsx
@@ -1,9 +1,10 @@
 /* eslint-disable  react-hooks/exhaustive-deps */
-import { Container, Heading } from "@chakra-ui/react";
+import { Heading } from "@chakra-ui/react";
 import { memo, useEffect, VFC } from "react";
 import { useLocation } from "react-router-dom";
 
 import { useJudgeResult } from "../hooks/useJudgeResult";
+import { ResultTable } from "../molecules/ResultTable";
 import { ContestType } from "../types/contest";
 
 type CustomizedState = {
@@ -25,22 +26,11 @@ export const ResultKing: VFC = memo(() => {
   const judgeResults2 = judgeResults.slice();
   const DeletejudgeResults = judgeResults2.splice(0, 3)
 
-  // console.log(judgeResults[1].score) //松本人志、滝音、点数
-  // console.log(judgeResults[51].score) //ログインユーザー、滝音、点数
-  // console.log(judgeResults)
-
-  console.log(other)
-  console.log(finalists)
-  console.log(judges)
-  console.log(judgeResults2)
-
   return (
     <>
-    <Heading align='center' mb={6}>結果発表〜〜〜！！！</Heading>
+    <Heading pt={5} align='center' mb={6}>結果発表〜〜〜！！！</Heading>
     {judgeResults.length !== 0 && (
-      <Container align='center'>
-        
-      </Container>
+      <ResultTable other={other} finalists={finalists} judges={judges} judgeResults2={judgeResults2} />
     )}
     </>
   )

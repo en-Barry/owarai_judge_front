e58cb2ddb4d90f56233c58b5c73d1c12216c14e6
en-Barry <en_barry@icloud.com>
2021-09-29 12:38:31 +0900

    Fix: 審査完了ボタンが正しく機能するように修正
    投票が完了するまでローディング中にするため

--- src/components/molecules/JudgeButtons.tsx
+++ src/components/molecules/JudgeButtons.tsx
@@ -14,13 +14,11 @@ export const JudgeButtons: VFC<Props> = memo((props) => {
   const { pathname, contest } = props;
 
   const history = useHistory();
-  const onClickJudgement = useCallback(() => history.push(`${pathname}`), []);
   const onClickResult = useCallback((contest) => history.push({ pathname: `${pathname}`, state: { contest: contest }}), []);
   const onClickBack = useCallback(() => history.goBack(), []);
 
   return (
     <>
-      <Button w='80%' mt={4} mb={6} p={6} fontSize='lg' onClick={onClickJudgement}>審査完了！</Button>
       <Button w='80%' mb={6} p={6} fontSize='lg' onClick={() => onClickResult(contest)}>結果だけ見る</Button>
       <Button w='80%' mb={6} p={6} fontSize='lg' onClick={onClickBack}>前の画面に戻る</Button>
     </>

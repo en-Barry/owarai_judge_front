d9b4c88d3eea1c3512ae601f746fba91a1a9225d
en-Barry <en_barry@icloud.com>
2021-09-29 14:32:52 +0900

    Change: BaseButtonをstyled-componentsを使わない形に変更
    本番環境でエラーが発生するため

--- src/components/molecules/SelectYearButtons.tsx
+++ src/components/molecules/SelectYearButtons.tsx
@@ -1,8 +1,8 @@
 /* eslint-disable  react-hooks/exhaustive-deps */
-import { memo, useCallback, VFC } from "react";
+import { Button, ButtonProps } from "@chakra-ui/react";
+import { FC, memo, useCallback, VFC } from "react";
 import { useHistory } from "react-router-dom";
 
-import { BaseButton } from "../atoms/button/BaseButton";
 import { ContestType } from "../types/contest";
 
 type Props = {
@@ -18,14 +18,15 @@ export const SelectYearButtons: VFC<Props> = memo((props) => {
   const history = useHistory();
   const onClickYear = useCallback((contest) => history.push({ pathname: `${pathname}`, state: { contest: contest }}), []);
 
+  const YButton : FC<ButtonProps> = (props) => <Button width='80%' style={{color: `#${color}`, backgroundColor: `#${bgColor}`, marginBottom: '1rem'}} {...props} />
+
   return (
     <>
       {contests.map((contest) => (
         <>
-        <BaseButton style={{color: `#${color}`, backgroundColor: `#${bgColor}`, marginBottom: '1rem'}}
-                    key={contest.id} onClick={() => onClickYear(contest)}>
+        <YButton key={contest.id} onClick={() => onClickYear(contest)}>
           {contest.year}
-        </BaseButton>
+        </YButton>
         </>
       ))}
     </>

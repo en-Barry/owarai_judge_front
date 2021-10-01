09cfb952a6af1ae14b4c8d40edb05c100fadb0fc
en-Barry <en_barry@icloud.com>
2021-10-01 23:46:27 +0900

    Fix: metaタグの設定をindex.htmlに書き換え
    react-helmetに記述してもOGPが正しく設定されないため

--- src/App.tsx
+++ src/App.tsx
@@ -8,25 +8,7 @@ import theme from './theme/theme';
 export default function App() {
   return (
     <>
-    <Helmet
-      title={'オワライジャッジ.com'}
-      meta={[
-        { name: 'description', content: 'お笑い賞レースの審査員アプリ' },
-        { name: "viewport", content: "width=device-width, initial-scale=1" },
-        { property: 'og:title', content: 'オワライジャッジ.com'},
-        { property: 'og:type', content: 'website'},
-        { property: 'og:url', content: 'https://www.owarai-judge.com' },
-        { property: 'og:image', content: `${process.env.PUBLIC_URL}/owarai-judge-bigogp.jpg` },
-        { property: 'og:description', content: 'お笑い賞レースの審査員アプリ' },
-        { name: 'twitter:title', content: 'オワライジャッジ.com' },
-        { name: 'twitter:descriptio', content: 'お笑い賞レースの審査員アプリ' },
-        { name: 'twitter:card', content: 'summary' },
-        { name: 'twitter:image', content: `${process.env.PUBLIC_URL}/owarai-judge-ogp.jpg` }
-      ]}
-      link={[
-        { 'rel': 'icon', 'type': 'image/jpg', 'href': `${process.env.PUBLIC_URL}/favicon.ico`}
-      ]}
-      >
+    <Helmet>
       <script async src="https://www.googletagmanager.com/gtag/js?id=G-HF2N44QBLF"></script>
       <script>
         {`

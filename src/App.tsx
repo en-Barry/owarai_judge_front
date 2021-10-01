f1da085990a2e0468590e6b399aa6716ededa969
en-Barry <en_barry@icloud.com>
2021-10-01 21:08:45 +0900

    Add: faviconとOGP画像を設定

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,20 @@ import theme from './theme/theme';
 export default function App() {
   return (
     <>
-    <Helmet>
-      <title>オワライジャッジ.com</title>
-      <meta name='description' content='お笑い賞レースの審査員なりきりアプリ' />
+    <Helmet
+      title={'オワライジャッジ.com'}
+      meta={[
+        { name: 'description', content: 'お笑い賞レースの審査員アプリ' },
+        { property: 'og:title', content: 'オワライジャッジ.com'},
+        { property: 'og:url', content: 'https://www.owarai-judge.com' },
+        { property: 'og:image', content: `${process.env.PUBLIC_URL}/owarai-judge-ogp.jpg` },
+        { property: 'og:description', content: 'お笑い賞レースの審査員アプリ' },
+        { name: 'twitter:card', content: 'summary' }
+      ]}
+      link={[
+        { 'rel': 'icon', 'type': 'image/jpg', 'href': ''}
+      ]}
+      >
       <script async src="https://www.googletagmanager.com/gtag/js?id=G-HF2N44QBLF"></script>
       <script>
         {`

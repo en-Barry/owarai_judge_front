1dd263da2f96c67721f789385ce3ca0ad435e2ed
ryusei_obama <en_barry@icloud.com>
2021-10-15 17:37:22 +0900

    Fix: 本番環境用のURLを記述
    環境ごとに使用するURLが違うため

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,4 +1,4 @@
-export const getContest = (props: string) => `http://localhost:3001/api/v1/contests/${props}`
-export const getFinalist = (props: string) => `http://localhost:3001/api/v1/finalists/${props}`
-export const getJudgement = (props: string) => `http://localhost:3001/api/v1/judgements/${props}`
-export const postJudgements= () => `http://localhost:3001/api/v1/judgements`
+export const getContest = (props: string) => `https://owarai-judge-api-back.herokuapp.com/api/v1/contests/${props}`
+export const getFinalist = (props: string) => `https://owarai-judge-api-back.herokuapp.com/api/v1/finalists/${props}`
+export const getJudgement = (props: string) => `https://owarai-judge-api-back.herokuapp.com/api/v1/judgements/${props}`
+export const postJudgements = () => `https://owarai-judge-api-back.herokuapp.com/api/v1/judgements`
